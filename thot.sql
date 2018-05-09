a435685b4ffac74628abed908041c40559937c6f
Franklin Esquivel <franklin.esquivel@outlook.com>
2018-05-08 22:02:13 -0600

    Vista 'Historial'

--- thot.sql
+++ thot.sql
@@ -200,6 +200,23 @@ DELIMITER ;
 /*!50003 SET collation_connection  = @saved_col_connection */ ;
 
 --
+-- Temporary view structure for view `historial`
+--
+
+DROP TABLE IF EXISTS `historial`;
+/*!50001 DROP VIEW IF EXISTS `historial`*/;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+/*!50001 CREATE VIEW `historial` AS SELECT 
+ 1 AS `idProceso`,
+ 1 AS `inicio`,
+ 1 AS `limite`,
+ 1 AS `estado`,
+ 1 AS `tipoProceso`,
+ 1 AS `idUsuario`*/;
+SET character_set_client = @saved_cs_client;
+
+--
 -- Table structure for table `imprenta`
 --
 
@@ -876,6 +893,24 @@ DELIMITER ;
 /*!50001 SET character_set_client      = @saved_cs_client */;
 /*!50001 SET character_set_results     = @saved_cs_results */;
 /*!50001 SET collation_connection      = @saved_col_connection */;
+
+--
+-- Final view structure for view `historial`
+--
+
+/*!50001 DROP VIEW IF EXISTS `historial`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8 */;
+/*!50001 SET character_set_results     = utf8 */;
+/*!50001 SET collation_connection      = utf8_general_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `historial` AS select `p`.`idPrestamo` AS `idProceso`,`p`.`fecha_prestamo` AS `inicio`,`p`.`fecha_devolucion` AS `limite`,`p`.`estado` AS `estado`,'P' AS `tipoProceso`,`u`.`idUsuario` AS `idUsuario` from (`prestamo` `p` join `usuario` `u` on((`u`.`idUsuario` = `p`.`idUsuario`))) union select `r`.`idReserva` AS `idProceso`,`r`.`fecha_reserva` AS `inicio`,`r`.`fecha_vencimiento` AS `limite`,`r`.`estado` AS `estado`,'R' AS `tipoProceso`,`u`.`idUsuario` AS `idUsuario` from (`reserva` `r` join `usuario` `u` on((`u`.`idUsuario` = `r`.`idUsuario`))) */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -896,4 +931,4 @@ CREATE EVENT `evento_verificar_procesos`
   DO
 	CALL thot.verificar_procesos()
 
--- Dump completed on 2018-05-03 21:54:41
+-- Dump completed on 2018-05-08 21:57:18
